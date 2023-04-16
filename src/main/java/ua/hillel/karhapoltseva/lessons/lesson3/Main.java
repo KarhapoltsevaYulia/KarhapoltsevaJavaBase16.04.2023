ffe28bbcf3b07041dcc6197d6a52d46dbc0677ce
Karhapoltseva Yaliya <nnnnnn493@gmail.com>
2023-04-16 11:00:10 +0300

    test push

--- src/main/java/ua/hillel/karhapoltseva/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/karhapoltseva/lessons/lesson3/Main.java
@@ -3,5 +3,10 @@ package ua.hillel.karhapoltseva.lessons.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lesson 3");
+        System.out.println("Lesson 3");
+        System.out.println("Lesson 3");
+        System.out.println("Lesson 3");
+        System.out.println("Lesson 3");
+        System.out.println("Lesson 3");
     }
 }
