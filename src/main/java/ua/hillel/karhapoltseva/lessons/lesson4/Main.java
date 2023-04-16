df5dc6aea13a2bfbb48b02fe836e415af06006a9
Karhapoltseva Yaliya <nnnnnn493@gmail.com>
2023-04-16 20:56:09 +0300

    comit all filles

---
+++ src/main/java/ua/hillel/karhapoltseva/lessons/lesson4/Main.java
@@ -0,0 +1,14 @@
+package ua.hillel.karhapoltseva.lessons.lesson4;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 10;
+        int volume = a*b*c;
+        System.out.println(" Об'ем паралеліпіпеда = "+ volume);
+
+    }
+
+
+}
