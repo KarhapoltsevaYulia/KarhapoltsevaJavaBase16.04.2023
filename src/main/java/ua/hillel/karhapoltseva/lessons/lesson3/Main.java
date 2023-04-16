966c3904c6a5e7348c83f9f7c48b3ebec57bea37
Karhapoltseva Yaliya <nnnnnn493@gmail.com>
2023-04-16 11:03:00 +0300

    test comit

--- src/main/java/ua/hillel/karhapoltseva/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/karhapoltseva/lessons/lesson3/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Lesson 3");
         System.out.println("Lesson 3");
         System.out.println("Lesson 3");
+        System.out.println("Lesson 3");
     }
 }
