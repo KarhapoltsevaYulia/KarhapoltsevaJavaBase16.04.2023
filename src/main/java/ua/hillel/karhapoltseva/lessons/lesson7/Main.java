4a69f0e6a88d6cc37ddc8aab7baae0f3036c07ee
Karhapoltseva Yaliya <nnnnnn493@gmail.com>
2023-04-30 13:45:17 +0300

    comit DZ 7

--- src/main/java/ua/hillel/karhapoltseva/lessons/lesson7/Main.java
+++ src/main/java/ua/hillel/karhapoltseva/lessons/lesson7/Main.java
@@ -9,27 +9,46 @@ public class Main {
         int[] team1 = new int[25];
         int[] team2 = new int[25];
         int minAge = 18, maxAge = 40;
-        double sumTeam1 = 0,sumTeam2 = 0;
+        double sumTeam1 = 0,sumTeam2 = 0, averageTeam1=0,averageTeam2=0;
+     if (team1.length==team2.length) {
+         for (int i = 0; i < team1.length; i++) {
+             team1[i] = (int) (Math.random() * (maxAge - minAge + 1) + minAge);
+             sumTeam1 += team1[i];
+             System.out.print(team1[i] + " ");
+             team2[i] = (int) (Math.random() * (maxAge - minAge + 1) + minAge);
+             sumTeam2 += team2[i];
 
-        for (int i = 0; i < team1.length; i++) {
-               team1[i] = (int) (Math.random() * (maxAge - minAge + 1) + minAge);
-               sumTeam1 += team1[i];
-               System.out.print(team1[i] + " ");
-               team2[i] = (int) (Math.random() * (maxAge - minAge + 1) + minAge);
-               sumTeam2 += team2[i];
-
-             if (i==team1.length-1){
+             if (i == team1.length - 1) {
                  System.out.println();
-                 for (int value:team2 ) {
-                     System.out.print(value+" ");
+                     for (int value : team2) {
+                     System.out.print(value + " ");
+                     }
+                 averageTeam1 = sumTeam1 / team1.length;
+                 averageTeam2 = sumTeam2 / team2.length;
+                 System.out.println();
+                 System.out.println(" Середній вік гравців першої команди : " + averageTeam1);
+                 System.out.println(" Середній вік гравців другої команди : " + averageTeam2);
                  }
-                 double averageTeam1= sumTeam1/team1.length;
-                 double averageTeam2= sumTeam2/team2.length;
-                 System.out.println();
-                 System.out.println(" Середній вік гравців першої команди : "+averageTeam1);
-                 System.out.println(" Середній вік гравців другої команди : "+averageTeam2);
-             }
-        }
+            }
+    } else {
+         for (int i = 0; i < team1.length; i++) {
+             team1[i] = (int) (Math.random() * (maxAge - minAge + 1) + minAge);
+             sumTeam1 += team1[i];
+             System.out.print(team1[i] + " ");
+         }
+         System.out.println();
+         for (int i = 0; i < team2.length; i++) {
+             team2[i] = (int) (Math.random() * (maxAge - minAge + 1) + minAge);
+             sumTeam2 += team2[i];
+             System.out.print(team2[i] + " ");
+         }
+         averageTeam1 = sumTeam1 / team1.length;
+         averageTeam2 = sumTeam2 / team2.length;
+         System.out.println();
+         System.out.println(" Середній вік гравців першої команди : " + averageTeam1);
+         System.out.println(" Середній вік гравців другої команди : " + averageTeam2);
+
+}
     }
 }
 /*Є дві команди регбі з 25 гравців різного віку у кожній.
