62a0d3584c1f21cbf6e838c05a5a99a072c7f669
Karhapoltseva Yaliya <nnnnnn493@gmail.com>
2023-05-25 16:57:44 +0300

    comit 13.1

--- src/main/java/ua/hillel/karhapoltseva/lessons/lesson13/Main.java
+++ src/main/java/ua/hillel/karhapoltseva/lessons/lesson13/Main.java
@@ -2,7 +2,7 @@ package ua.hillel.karhapoltseva.lessons.lesson13;
 
 public class Main {
     public static void main(String[] args) {
-        MusicStyles[] musicStyles ={
+      /*  MusicStyles[] musicStyles ={
                new  PopMusic("Akcent"),
                new  PopMusic("Backstreet Boys"),
                new  PopMusic("Aqua"),
@@ -14,11 +14,38 @@ public class Main {
                 new ClassicMusic("Beethoven"),
                 new ClassicMusic("Bax"),
                 new ClassicMusic("Mozart"),
+        };*/
+
+        PopMusic[] popMusic = {
+                new  PopMusic("Akcent"),
+                new  PopMusic("Backstreet Boys"),
+                new  PopMusic("Aqua")
+        };
+        RockMusic[] rockMusics ={
+                new RockMusic("Mad Heads"),
+                new RockMusic("Esthetic Education"),
+                new RockMusic("Green Grey")
+        };
+        ClassicMusic[] classicMusics={
+                new ClassicMusic("Beethoven"),
+                new ClassicMusic("Bax"),
+                new ClassicMusic("Mozart")
         };
 
-        for (MusicStyles musicStyle : musicStyles) {
-            musicStyle.playMusic();
+        for (PopMusic music : popMusic) {
+            music.playMusic();
         }
+        for (RockMusic rockMusic : rockMusics) {
+            rockMusic.playMusic();
+        }
+        for (ClassicMusic classicMusic : classicMusics) {
+            classicMusic.playMusic();
+        }
+
+      /*  for (MusicStyles musicStyle : musicStyles) {
+            musicStyle.playMusic();
+        }*/
+
     }
 }
 
---
+++ src/main/java/ua/hillel/karhapoltseva/lessons/lesson14/Smartphones.java
@@ -0,0 +1,7 @@
+package ua.hillel.karhapoltseva.lessons.lesson14;
+
+public interface Smartphones {
+    public void call();
+    public void sms();
+    public void internet();
+}
