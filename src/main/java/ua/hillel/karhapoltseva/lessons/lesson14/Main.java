64faa54d0d5801f10f605c257ea57d9feb8a8aba
Karhapoltseva Yaliya <nnnnnn493@gmail.com>
2023-05-27 21:08:56 +0300

    comit 14

--- src/main/java/ua/hillel/karhapoltseva/lessons/lesson14/Main.java
+++ src/main/java/ua/hillel/karhapoltseva/lessons/lesson14/Main.java
@@ -2,14 +2,39 @@ package ua.hillel.karhapoltseva.lessons.lesson14;
 
 public class Main {
     public static void main(String[] args) {
-
         objectAndroids();
         objectIphones();
         objectSmarthones();
         objectLinuxOs();
         objectIos();
 
+        Ios[] iosMas = {
+              new Iphones(),
+        };
 
+         LinuxOs[] linuxOs ={
+              new Androids(),
+         };
+
+         Smartphones[] smartphones = {
+              new Iphones(),
+              new Androids()
+         };
+
+        for (Ios ios1:iosMas) {
+            ios1.qauickCommandsIos();
+            ios1.appleIdIos();
+        }
+
+        for (LinuxOs linuxOs1:linuxOs) {
+            linuxOs1.systemLinuxOs();
+        }
+
+        for (Smartphones smartphones1:smartphones) {
+            smartphones1.sms();
+            smartphones1.call();
+            smartphones1.internet();
+        }
     }
 
     private static void objectIos() {
@@ -18,7 +43,6 @@ public class Main {
             public void qauickCommandsIos() {
                 System.out.println("Ios object qauickCommandsIos");
             }
-
             @Override
             public void appleIdIos() {
                 System.out.println("Ios object appleIdIos");
@@ -43,12 +67,10 @@ public class Main {
             public void call() {
                 System.out.println("Smartphones object Call!");
             }
-
             @Override
             public void sms() {
                 System.out.println("Smartphones object sms!");
             }
-
             @Override
             public void internet() {
                 System.out.println("Smartphones object internet!");
