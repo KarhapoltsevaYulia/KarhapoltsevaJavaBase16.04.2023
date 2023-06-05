861c5267bae4c7b43f70529dfb0ab58f737f8e4e
Karhapoltseva Yaliya <nnnnnn493@gmail.com>
2023-06-05 17:20:29 +0300

    comit 16

---
+++ src/main/java/ua/hillel/karhapoltseva/lessons/lesson16/ArrayUtil.java
@@ -0,0 +1,56 @@
+package ua.hillel.karhapoltseva.lessons.lesson16;
+
+public class ArrayUtil {
+
+    // if null   return -2
+    // if empty  return -1
+    // if not found return -6
+    // if matrix [][] return 0 //є квадратною
+    // if matrix [][] return -5 //  не квадратнa
+
+    static double  methodArrayInt(int[] array){
+        double average=0;
+        if (array.length == 0){
+            return -1;
+        }
+        if (array == null){
+            return -2;
+        }
+        if (array.length>0) {
+            for (int i = 0; i < array.length; i++) {
+                average += array[i];
+            }
+            average = average / array.length;
+            System.out.println(average);
+            return average;
+        }
+        return -6;
+    }
+    static int  methodMatrix(int[][] array) {
+        if(array.length == 0){
+            return -1;
+        }
+        if (array==null){
+            return -2;
+        }
+
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    if ((array.length==array[i].length)&&array.length>1) {
+                        System.out.println("Матриця квадратна");
+                        return 0;
+                    } else  {
+                        if ((array.length!=array[i].length)) {
+                            System.out.println("Матриця не квадратна");
+                        return -5;
+                        }
+                    }
+                }
+            }
+            return -3;
+    }
+}
+/*Додайте метод, який буде рахувати середнє арифметичне елементів масиву.
+Зробіть метод, який прийматиме на вхід двомірну матрицю (двовимірний масив)
+у та перевіряти чи є вона квадратною
+В тестовому класі створіть тестову матрицю та передайте її в метод тестованого класу*/
---
+++ src/test/java/ua/hillel/karhapoltseva/lessons/lesson16/TestArrayUtil.java
@@ -0,0 +1,59 @@
+package ua.hillel.karhapoltseva.lessons.lesson16;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestArrayUtil {
+    static int[] array = null;
+    static int[][] arrayMatrix=null;
+    @BeforeAll
+    static void beforeAll() {
+        array = new int[]{
+
+        };// 1,2,3,4,5
+        arrayMatrix=new int[][]{
+                {},
+                {1, 2, 3, 4, 5}
+        };
+
+
+    }
+
+
+    @Test
+    void testmethod1(){
+        Assertions.assertEquals(-1,ArrayUtil.methodArrayInt(new int[]{}));
+    }
+    @Test
+    void testmethod2(){
+        Assertions.assertFalse(ArrayUtil.methodArrayInt(array)== -2);
+    }
+    @Test
+    void testmethod3(){
+        Assertions.assertEquals(-1,ArrayUtil.methodMatrix(new int[][]{}));
+    }
+    @Test
+    void testmethod4(){
+        Assertions.assertFalse(ArrayUtil.methodMatrix(arrayMatrix)== -2);
+    }
+    @Test
+    void testmethod5(){
+        Assertions.assertEquals(-5,ArrayUtil.methodMatrix(new int[5][4]));
+    }
+    @Test
+    void testmethod6(){
+        Assertions.assertEquals(-5,ArrayUtil.methodMatrix(arrayMatrix));
+    }
+    @Test
+    void testmethod7(){
+        Assertions.assertEquals(0,ArrayUtil.methodMatrix(new int[5][5]));
+    }
+
+    @AfterAll
+    static void afterAll() {
+        array=null;
+        arrayMatrix = null;
+    }
+
+}
